0a1a1bb76623aa09ebc46e7fb3f8d6cddfa45395
arunvemireddy <vemireddyarun0701@gmail.com>
2022-05-25 14:28:30 -0600

    fc

---
+++ date.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+dd=`date +%m-%d-%Y`
+echo $dd
+d=`date +%m-%Y`
+echo $d #MM-DD_YYYY
+w=`date '+%A %d-%B, %Y'`
+echo $w
---
+++ sleep.sh
@@ -0,0 +1,6 @@
+# start time
+date +"%H:%M:%S"
+echo "wait for 9  seconds"
+sleep 9s
+date +"%H:%M:%S"
+echo "Task Completed"
